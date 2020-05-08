80f4263461451dd8174df907c18894467e767f7a
Brillan <46096098+bpm150@users.noreply.github.com>
2020-05-07 20:30:39 -0700

    Refactored Problem1 and Problem2 into their own classes and files. Problem1 still works. Test cases set up for Problem2

---
+++ Problem2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment4
+{
+    public static class Problem2
+    {
+        public static void RunTests()
+        {
+            var testCases = new List<TestCase>
+            {
+                new TestCase
+                {
+                    intList = new List<int>{ 2 },
+                    missingInt = 1,
+                },
+                new TestCase
+                {
+                    intList = new List<int>{ 1, 3 },
+                    missingInt = 2,
+                },
+                new TestCase
+                {
+                    intList = new List<int>{ 3, 2, 1 },
+                    missingInt = 4,
+                },
+                new TestCase
+                {
+                    intList = new List<int>{ 4, 1, 5, 3 },
+                    missingInt = 2,
+                },
+                new TestCase
+                {
+                    intList = new List<int>{ 6, 3, 4, 2, 1 },
+                    missingInt = 5,
+                },
+                new TestCase
+                {
+                    intList = new List<int>{ 5, 2, 4, 3, 6, 7 },
+                    missingInt = 1,
+                },
+            };
+
+
+
+
+
+
+         }
+
+        private class TestCase
+        {
+            //public TestCase(int[] arr, int sum)
+            //{
+            //    testArray = arr; // Need to read up on C# arrays...do I need to allocate for this array?
+            //    largestSum = sum;
+            //}
+
+            public List<int> intList { get; set; }
+            public int missingInt { get; set; }
+        }
+
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace Assignment4
+{
+    public static class Utility
+    {
+        public static string ArrayToString<T>(T[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 0)
+            {
+                return "{ }";
+                // Avoids awkwardly removing an extra space later
+            }
+
+            var builder = new StringBuilder("{ ");
+
+            foreach (var item in array)
+            {
+                builder.Append($"{item}, ");
+            }
+
+            var length = builder.Length;
+            builder.Remove(length - 2, 2).Append(" }");
+
+            return builder.ToString();
+
+        }
+        private static void Test_ArrayToString()
+        {
+            // Test ArrayToString for
+            // null
+            // length == 0
+            // length == 1
+            // length == 2(+)
+
+
+
+            // (Permanently) empty array
+            // Arrays cannot change size
+            // Empty array in C# is as close as you can get to an immutable collection
+            // in C#
+            // Can use the same instance for all purposes, as you can with String.Empty
+
+            var intArray = new int[] { };
+            Console.WriteLine(ArrayToString(intArray));
+
+            intArray = new int[] { 42 };
+            Console.WriteLine(ArrayToString(intArray));
+
+            intArray = new int[] { 7, 11 };
+            Console.WriteLine(ArrayToString(intArray));
+
+            intArray = null;
+            //Console.WriteLine(ArrayToString(intArray));
+
+        }
+    }
+}
