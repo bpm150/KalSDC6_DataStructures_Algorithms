cbc3a5547281acc3d9b70a6d13a194f368494ef0
Brillan <46096098+bpm150@users.noreply.github.com>
2020-05-08 20:26:30 -0700

    Problem2 Updated test to include header and problem text Refactored testing code Included summary of succeeded and oopsed tests

--- Problem2.cs
+++ Problem2.cs
@@ -41,36 +41,82 @@ namespace Assignment4
                 },
             };
 
+            string intro =
+                "==============\n" +
+                "= Problem #2 =\n" +
+                "==============\n" +
+                "\n" +
+                "You are given a list of n-1 integers and these integers are in the range of 1 to n.\n" +
+                "There are no duplicates in list.\n" +
+                "One of the integers is missing in the list.\n" +
+                "Write an efficient code to find the missing integer.";
+
+            Console.WriteLine(intro);
+
+            int testOopsCount = 0;
 
             for (var i = 0; i < testCases.Count; ++i)
             {
-                Console.WriteLine($"Test #{i + 1}:");
+                Console.WriteLine($"\nTest #{i + 1}:");
 
-                var testCaseResult = FindMissingIntegerFromUnique(testCases[i].intList);
+                var listCount = testCases[i].intList.Count;
 
                 Console.WriteLine($"For the list: {Utility.CollectionToString(testCases[i].intList)}");
+                Console.WriteLine($"n == { listCount + 1 }");
+                Console.WriteLine($"n-1 == { listCount }");
                 Console.WriteLine($"The the missing integer is {testCases[i].missingInt}.");
 
-                var resultMessage = testCaseResult == testCases[i].missingInt ? "SUCCESS" : "OOPS";
+                var testCaseResult = FindMissingInteger(testCases[i].intList);
 
-                Console.WriteLine($"{resultMessage}! Your answer is {testCaseResult}.\n");
+                string resultMessage;
+
+                if (testCaseResult == testCases[i].missingInt)
+                {
+                    resultMessage = "SUCCESS";
+                }
+                else
+                {
+                    ++testOopsCount;
+                    resultMessage = "OOPS";
+                }
+
+                Console.WriteLine($"{resultMessage}! Your answer is {testCaseResult}.");
             }
 
+            var testCount = testCases.Count;
+            var testSuccessCount = testCount - testOopsCount;
+
+            Console.WriteLine($"\n\nOut of {testCount} tests total,\n");
+            Console.WriteLine($"{testSuccessCount}/{testCount} tests succeeded, and");
+            Console.WriteLine($"{testOopsCount}/{testCount} tests oopsed.\n");
+
+            if (testOopsCount == 0)
+            {
+                Console.WriteLine($"YAY! All tests succeeded! :D\n");
+            }
         }
 
-        public static int FindMissingIntegerFromUnique(List<int> intList)
+        public static int FindMissingInteger(List<int> intList)
         {
             var n = intList.Count + 1;
 
             // Triangluar numbers formula
-            var sumOfRange = (n * (n + 1)) / 2;
+            // https://en.wikipedia.org/wiki/Triangular_number
+            var sumOfAllPossibleValues = (n * (n + 1)) / 2;
             // I can see why >> 2 doesn't work on the inside, but why doesn't it work on the outside?
 
             foreach (var num in intList)
-                sumOfRange -= num;
+                sumOfAllPossibleValues -= num;
 
-            return sumOfRange;
+            // All that's left after subtracting the found integers is
+            // a value equal to the missing one
+            return sumOfAllPossibleValues;
+
+            // Solved in linear time complexity: O(n)
+            // and in constant space complexity: O(1)
+            //...which, for algorithms, is pretty much the best you can hope for!
         }
+
 
         private class TestCase
         {
