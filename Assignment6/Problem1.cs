4ce3ac66eb41888462df736e274299e8ac65c220
Brillan <46096098+bpm150@users.noreply.github.com>
2020-06-12 23:51:54 -0700

    Eliminated longestLen variable, now using longestPal.Length Eliminated isPal variable Simplified if (s.Length - j - 1 < subsLen - 1) by adding 1 to both sides of the inequality

--- Assignment6/Problem1.cs
+++ Assignment6/Problem1.cs
@@ -8,33 +8,29 @@ namespace Assignment6
     {
         public static void RunTests()
         {
+            // FIX TACOCAT BUG
             var testCases = new List<TestCase>
             {
-                //new TestCase
-                //{
-                //    CorrectOutputInt = -1,
-                //    RomanNumeralString = String.Empty,
-                //},
-                //new TestCase
-                //{
-                //    CorrectOutputInt = -1,
-                //    RomanNumeralString = "notaromannumeraleither",
-                //},
                 new TestCase
                 {
-                    CorrectFirstPalindrome = "bab",
+                    CorrectFirstLongestPalindrome = "FOOF",
+                    InputString = "babadFOOF",
+                },
+                new TestCase
+                {
+                    CorrectFirstLongestPalindrome = String.Empty,
+                    InputString = String.Empty,
+                },
+                new TestCase
+                {
+                    CorrectFirstLongestPalindrome = String.Empty,
+                    InputString = "a",
+                },
+                new TestCase
+                {
+                    CorrectFirstLongestPalindrome = "bab",
                     InputString = "babad",
                 },
-                //new TestCase
-                //{
-                //    CorrectFirstPalindrome = 12,
-                //    InputString = "XII",
-                //},
-                //new TestCase
-                //{
-                //    CorrectFirstPalindrome = 3999,
-                //    InputString = "MMMCMXCIX",
-                //},
             };
 
             string intro =
@@ -52,14 +48,14 @@ namespace Assignment6
             {
                 Console.WriteLine($"\nTest #{i + 1}:");
 
-                Console.WriteLine($"Input: { testCases[i].InputString }");
-                Console.WriteLine($"Output: { testCases[i].CorrectFirstPalindrome }");
+                Console.WriteLine($"Input: \"{ testCases[i].InputString }\"");
+                Console.WriteLine($"Output: \"{ testCases[i].CorrectFirstLongestPalindrome }\"");
 
                 var testCaseResult = LongestPalindrome(testCases[i].InputString);
 
                 string resultMessage;
 
-                if (testCaseResult == testCases[i].CorrectFirstPalindrome)
+                if (testCaseResult == testCases[i].CorrectFirstLongestPalindrome)
                 {
                     resultMessage = "SUCCESS";
                 }
@@ -69,7 +65,7 @@ namespace Assignment6
                     resultMessage = "OOPS";
                 }
 
-                Console.WriteLine($"{resultMessage}! Your answer is {testCaseResult}.");
+                Console.WriteLine($"{resultMessage}! Your answer is \"{testCaseResult}\".");
             }
 
             var testCount = testCases.Count;
@@ -88,62 +84,32 @@ namespace Assignment6
         private class TestCase
         {
             public string InputString { get; set; }
-            public string CorrectFirstPalindrome { get; set; }
+            public string CorrectFirstLongestPalindrome { get; set; }
         }
 
         public static string LongestPalindrome(string s)
         {
-            var longestLen = 0;
             var longestPal = String.Empty;
 
-            // This can go in the for loop after all
-            var i = 0;
             if (s == null)
                 throw new ArgumentNullException("string s is null");
 
-            // Despite my banter in the prelim vid,
-            // I think this may not be necessary after all:
-            if (s.Length == 1)
-                return String.Empty;
-            // (Because I decided that a single-character substring is not a palindrome)
-            // TODO: Decide for realsies if a single-character substring is a palindrome or not
+            // Empty string and string of length 1 don't need to be special-cased
+            // main logic correctly gives String.Empty as a result
 
-            for (; i < s.Length; ++i)
+            for (var i = 0; i < s.Length; ++i)
             {
-                if (longestLen >= s.Length - i)
+                if (longestPal.Length >= s.Length - i)
                     break;
 
                 for (var j = i + 1; j < s.Length; ++j)
                 {
                     var subsLen = j - i + 1;
-                    if (subsLen <= longestLen)
+                    if (subsLen <= longestPal.Length)
                         continue;
-                    var candPal = s.Substring(i, subsLen);
-                    var isPali = IsPalindrome(candPal);
-                    if (isPali && subsLen > longestLen)
-                    {
-                        longestLen = subsLen;
-                        longestPal = candPal;
-                    }
-                    // Inequality holds if you add 1 to both sides
-                    if (s.Length - j - 1 < subsLen - 1)
-                        break;
-                }
-            }
-            return longestPal;
-        }
 
-        private static bool IsPalindrome(string s)
-        {
-            var finalInd = s.Length - 1;
-            for (var i = 0; i <= finalInd; ++i)
-            {
-                // BUG: "+ cannot be applied to operands of type Index and int"
-                // (requires parenthases)
-                if (s[i] != s[^(i + 1)])
-                    return false;
-            }
-            return true;
-        }
-    }
-}
+                    var candPal = s.Substring(i, subsLen);
+                    if (IsPalindrome(candPal) && subsLen > longestPal.Length)
+                        longestPal = candPal;
+
+                    // THIS PART DOESN'T WORK (SEE DRAFT VIDEO FOR DISCUSSION)
---
+++ Assignment6/Problem2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6
+{
+    class Problem2
+    {
+
+
+    }
+}
