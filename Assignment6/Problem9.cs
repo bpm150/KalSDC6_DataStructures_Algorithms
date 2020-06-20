236acdf64050fecdd7e149da3b68c5c241b42334
Brillan <46096098+bpm150@users.noreply.github.com>
2020-06-20 05:34:56 -0700

    Restored explicit handling for an empty array Switched from linq to for loop for finding shortest string, also checking for null strings while at it

--- Assignment6/Problem9.cs
+++ Assignment6/Problem9.cs
@@ -19,7 +19,7 @@ namespace Assignment6
                     InputStrArr = new List<string>
                     {
                         "TOP",
-                       "TO",
+                        "TO",
                     },
                 },
                 new TestCase
@@ -87,12 +87,31 @@ namespace Assignment6
                         String.Empty
                     },
                 },
+                //new TestCase
+                //{
+                //    CorrectOutput = String.Empty,
+                //    InputStrArr = new List<string>
+                //    {},
+                //},
                 new TestCase
                 {
-                    CorrectOutput = String.Empty,
+                    CorrectOutput = "sweet",
                     InputStrArr = new List<string>
-                    {},
+                    {
+                        "SWEET",
+                        "SWEET"
+                    },
                 },
+                //new TestCase
+                //{
+                //    CorrectOutput = "sweet",
+                //    InputStrArr = new List<string>
+                //    {
+                //        "SWEET",
+                //        null,
+                //        "SWEET"
+                //    },
+                //},
             };
 
             string intro =
@@ -157,28 +176,98 @@ namespace Assignment6
                 throw new ArgumentNullException(
                     "parameter array is null");
 
+            if(strArr.Length == 0)
+                throw new ArgumentException(
+                    "parameter array is empty");
+
+            // Fixed more elegantly with FirstOrDefault() below
             // BUG:
             // Can't call First() on an empty sequence/collection
             // The for loops are safe, we reach for shortestStr first
-            if (strArr.Length < 1)
-                return String.Empty;
+            //if (strArr.Length == 0)
+            //    return String.Empty;
 
             // TODO:
             // OH! We still need to check and make sure each individual
             // string in the array is not null
+            // Executive decision is to skip over null strings
+            // Give the correct answer for all the non-null strings
+            // in the array
 
-            var shortestStr = strArr.OrderBy(str => str.Length).First();
-            var shortestLen = shortestStr.Length;
 
-            for (var j = 0; j < shortestLen; ++j)
+
+            // Nah...as cute as the linq is, it is turning out to be a hassle
+            // More trouble than it is worth, etc.
+            // Ask Linq to skip over any null strings
+            // We will throw for null strings, but can't do that inside a linq
+            // query...and even if we could...that sounds ugly, so let's not
+
+            //var shortestStr = String.Empty;
+            var shortestStr = String.Empty;
+            for (var k = 0; k < strArr.Length; ++k)
             {
+                if (strArr[k] == null)
+                    throw new ArgumentNullException(
+                        "param array contains a null string");
+
+                if (k == 0)
+                    shortestStr = strArr[k];
+                else if (strArr[k].Length < shortestStr.Length)
+                    shortestStr = strArr[k];
+            }
+
+            //var shortestStr = String.Empty;
+            //for(var k = 0; k < strArr.Length; ++k)
+            //{ 
+            //    if (strArr[k] == null)
+            //        throw new ArgumentNullException(
+            //            "param array contains a null string");
+
+            //    if (k == 0)
+            //        shortestStr = strArr[k];
+            //    else if (strArr[k].Length < shortestStr.Length)
+            //        shortestStr = strArr[k];
+            //}
+
+            //var shortestStr = String.Empty;
+            //foreach (var str in strArr)
+            //{
+            //    if(str == null)
+            //        throw new ArgumentNullException(
+            //            "param array contains a null string");
+
+            //    shortestStr = str.Length < shortestStr.Length ? str : shortestStr;
+            //}
+            //var shortestStr = strArr.Where(str => str != null).
+            //                            OrderBy(str => str.Length).
+            //                            FirstOrDefault();
+            // FIX: First() -> FirstOrDefault()
+            // If there strArr is empty, or contains only nulls
+            // Get String.Empty
+
+            for (var j = 0; j < shortestStr.Length; ++j)
+            {
+                // NVM:
+                // compCh will get properly assigned a value before being read from
+                // Compiler's static analysis suggests that this doesn't happen
+                // so have to assign something to it, even though that value
+                // never gets read
+                // Well, if strArr.Length == 0, the inner for loop would not
+                // execute, but then compCh would not get read from either,
+                // so idk
                 var compCh = Char.ToLower(strArr[0][j]);
                 for (var i = 1; i < strArr.Length; ++i)
                 {
-                    var ch = Char.ToLower(strArr[i][j]);
-                    if (compCh != ch)
+                // Normally would do this inside the outer loop
+                // and outside the inner loop,
+                // But need to check the strings in the array for null
+                // and right inside the inner loop is the right place
+                // to do that if you want to do it only once...like I do
+                    var currCh = Char.ToLower(strArr[i][j]);
+                    if (compCh != currCh)
                         // BUG:
-                        // Index/Range syntax is "..," not "..."
+                        // Index/Range syntax is:
+                        // ".." (two dots) not "..." (three dots)
                         // I think it was Ruby that had both two dots and
                         // three dots, where each meant something different
                         return strArr[0][..j].ToLower();
