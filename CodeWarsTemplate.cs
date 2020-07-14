ba3c138cad3f8cfca7086de4c7cc991dd9586611
Brillan <46096098+bpm150@users.noreply.github.com>
2020-07-13 21:00:21 -0700

    Pasted the solution and test templates for use with the CodeWars IDE

---
+++ CodeWarsTemplate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsTemplate
+{
+
+    // Link to IDE:
+    // https://www.codewars.com/kumite/5ef20849d2313b003357f9a6/edit
+
+    namespace AssignmentX
+    {
+        // Paste problem statement here
+
+        public static class ProblemY
+        {
+            // Paste algorithm description here
+
+            public static int SolutionMethod(int n)
+            {
+
+                return n;
+            }
+        }
+    }
+
+
+    // NUNIT TESTING TEMPLATE
+    //namespace Testing
+    //{
+    //    using NUnit.Framework;
+    //    using AssignmentX;
+    //    using static AssignmentX.ProblemY;
+
+    //    [TestFixture]
+    //    public class Tests
+    //    {
+    //        [Test]
+    //        public void SampleTest()
+    //        {
+    //            var input = 2;
+    //            var expectedOutput = input;
+
+    //            Assert.AreEqual(expectedOutput, SolutionMethod(input));
+    //        }
+    //    }
+    //}
+
+
+
+}
